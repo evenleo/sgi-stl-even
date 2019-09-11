a079e40c063a3b3a213b24d167840a99b6abb62f
evenleo <wuyingwen@qq.com>
2019-09-11 20:31:59 +0800

    删除跳表实现

--- main.cpp
+++ main.cpp
@@ -4,28 +4,13 @@
 #include "stl_alloc.h"
 #include "stl_iterator.h"
 #include "stl_list.h"
-#include "skiplist.h"
 
 using namespace std;
 
 
 int main()
 {
-    skiplist<int> ii;
-    for(int i=1; i< 50; i++){
-        if((i%3) == 0){
-            ii.insert(i);
-        }
-    }
-    for(int i=1; i< 50; i++){
-        if((i%3) == 1){
-            ii.insert(i);
-        }
-    }
-    ii.printAll();
-    std::cout << std::endl;
-    ii.printAll(-1);
-
+    cout << "hello" << endl;
     system("pause");
     return 0;
 }
