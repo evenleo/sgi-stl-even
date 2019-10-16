ff39b75c19e198027590aed6ae8fcd7219c8d9be
evenleo <wuyingwen@qq.com>
2019-10-17 07:34:44 +0800

    添加红黑树的实现和仿函数的实现

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@
 #include "stl_algobase.h"
 #include "stl_uninitialized.h"
 #include "stl_vector.h"
-#include "stl_list.h"
+#include "stl_list.h" 
 #include "stl_bvector.h"
+#include "stl_function.h"
+#include "stl_tree.h"
 
 // #include <vecotor_sgi>
 
@@ -67,14 +69,57 @@ void vector_test() {
 }
 
 void bit_vector_test() {
-    std::cout << "sizeof(bool):" << sizeof(bool) << std::endl;
+    std::cout << "sizeof(bool):" << sizeof(unsigned int) << std::endl;
+    std::cout << CHAR_BIT << std::endl;
+    int WORD_BIT = int(CHAR_BIT*sizeof(unsigned int));
+    std::cout << WORD_BIT << std::endl;
     STD::vector<bool> vec;
+    vec.push_back(true);
+    vec.push_back(false);
+    vec.push_back(true);
+
+    vec.push_back(false);
     vec.push_back(true);
     vec.push_back(false);
     for (auto i : vec) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
+}
+
+void stl_tree_test() {
+    typedef STD::rb_tree<int, int, STD::identity<int>, STD::less<int>> RB_tree;
+    RB_tree itree;
+    std::cout << itree.size() << std::endl;
+    itree.insert_unique(10);
+    itree.insert_unique(7);
+    itree.insert_unique(8);
+
+    itree.insert_unique(15);
+    itree.insert_unique(5);
+    itree.insert_unique(6);
+
+    itree.insert_unique(11);
+
+    itree.insert_unique(7);
+
+    itree.insert_unique(13);
+    itree.insert_unique(12);
+
+    std::cout << itree.size() << std::endl;
+    RB_tree::iterator it = itree.begin();
+    for ( ; it != itree.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    for (it = itree.begin(); it != itree.end(); ++it) {
+        STD::__rb_tree_base_iterator rbtite = STD::__rb_tree_base_iterator(it);
+        std::cout << *it << "(" << rbtite.node->color << ") ";
+    }
+    std::cout << std::endl;
+
+
 }
 
 void bool_ptr_test() {
@@ -86,8 +131,13 @@ void bool_ptr_test() {
 
 int main()
 {
-    bit_vector_test();
-    // bool_ptr_test();
+    STD::less<int> key_compare;
+    bool r1 = key_compare(2, 3);
+    bool r2 = key_compare(3, 3);
+    bool r3 = key_compare(4, 3);
+
+
+    // stl_tree_test();
 
     system("pause");
     return 0;
