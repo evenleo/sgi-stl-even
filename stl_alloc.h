a1b02ed1f55ed37a87468683e499a4e8d9ea7f84
even <wuyingwen@qq.com>
2019-10-28 14:08:37 +0800

    注释stl_alloc.h

--- stl_alloc.h
+++ stl_alloc.h
@@ -111,15 +111,15 @@ public:
 template<bool threads, int inst>
 class __default_alloc_template {
 private:
-	enum { ALIGN = 8 };
-	enum { MAX_BYTES = 128 };
-	enum { NFREELISTS = MAX_BYTES / ALIGN };  
+	enum { ALIGN = 8 };                       //区间间隔字节数
+	enum { MAX_BYTES = 128 };                 //最大字节数
+	enum { NFREELISTS = MAX_BYTES / ALIGN };  //自由链表数组的长度
 
-	static size_t ROUND_UP(size_t bytes) {
+	static size_t ROUND_UP(size_t bytes) {    //向上取8的倍数
 		return ((bytes + (size_t)ALIGN - 1) & ~(ALIGN - 1));
 	}
 
-	static size_t FREELIST_INDEX(size_t bytes) {
+	static size_t FREELIST_INDEX(size_t bytes) {  //获取所在自由链表数组的索引
 		return ((bytes + (size_t)ALIGN - 1) / (size_t)ALIGN - 1);
 	}
 
@@ -130,46 +130,49 @@ private:
 
 	static obj* volatile freeLists[NFREELISTS];
 
-	static char *chunk_alloc(size_t size, int& nobj);
-	static void *refill(size_t __n);
+	static char *chunk_alloc(size_t size, int& nobj);  //配置size*nobjs大小的区块
+	static void *refill(size_t __n);  //补充链表并返回一个有效的期望区块
 
-	static char *start_free;
-	static char *end_free;
+	static char *start_free;  //内存池起始位置，只在chunk_alloc()中变化
+	static char *end_free;    //内存池结束位置，只在chunk_alloc()中变化
 	static size_t heap_size;
 
 public:
-
+    
+    //配置空间
 	static void *allocate(size_t n) {
 		obj *volatile *my_free_list;
 		obj *result;
 
-		if (n > size_t(MAX_BYTES)) {
+		if (n > size_t(MAX_BYTES)) {  //大于128bytes，调用第一级配置器allocate()
 			return malloc_alloc::allocate(n);
 		}
 
-		my_free_list = freeLists + FREELIST_INDEX(n);
-		result = *my_free_list;
-		if (result == 0) {
-			void *r = refill(ROUND_UP(n));
+		my_free_list = freeLists + FREELIST_INDEX(n);  //定位到对应自由链表
+		result = *my_free_list;  //取当前链表的头结点
+		if (result == 0) {       //如果取值为空
+			void *r = refill(ROUND_UP(n));  //调用refill()函数
 			return r;
 		}
-
-		*my_free_list = result->free_list_link;
+        
+        //如果取值不为空，则自由链表指向下一个节点
+		*my_free_list = result->free_list_link;  
 		return result;
 	}
-
+    
+    //释放空间
 	static void deallocate(void* p, size_t n) {
-		obj *q = (obj*)p;
+		obj *q = (obj*)p;  //q指向待释放的区块
 		obj *volatile *my_free_list;
 
-		if (n > size_t(MAX_BYTES)) {
+		if (n > size_t(MAX_BYTES)) {  //大于128bytes，调用第一级配置的deallocate()
 			malloc_alloc::deallocate(p, n);
 			return;
 		}
 
-		my_free_list = freeLists + FREELIST_INDEX(n);
-		q->free_list_link = *my_free_list;
-		*my_free_list = q;
+		my_free_list = freeLists + FREELIST_INDEX(n);  //定位到对应自由链表
+		q->free_list_link = *my_free_list;  //待释放区块节点指向原自由链表的头结点
+		*my_free_list = q;  //自由链表的头结点指向q，上述过程既是头插法插入q节点
 	}
 
 	static void *reallocate(void *p, size_t old_sz, size_t new_sz);
@@ -178,6 +181,7 @@ public:
 typedef __default_alloc_template<__NODE_ALLOCATOR_THREADS, 0> alloc;  //二级配置器全局变量
 typedef __default_alloc_template<false, 0> single_client_alloc;
 
+//配置size*nobjs大小的区块，只被refill()函数调用
 template<bool threads, int inst>
 char *__default_alloc_template<threads, inst>::chunk_alloc(size_t size, int& nobjs)
 {
@@ -185,25 +189,26 @@ char *__default_alloc_template<threads, inst>::chunk_alloc(size_t size, int& nob
     size_t total_bytes = size * nobjs;
     size_t bytes_left = end_free - start_free;
 
-    if (bytes_left >= total_bytes) {
-        result = start_free;
+    if (bytes_left >= total_bytes) {  //如果内存池剩余空间大小大于等于total_bytes
+        result = start_free;          //直接取空间并返回
         start_free += total_bytes;
         return result;
-    } else if (bytes_left >= size) {
-        nobjs = bytes_left/size;
-        total_bytes = size * nobjs;
+    } else if (bytes_left >= size) {  //如果内存池剩余空间[size, total_bytes)
+        nobjs = bytes_left/size;      //取一个及以上区块，尽量多
+        total_bytes = size * nobjs;   //然后取空间并返回
         result = start_free;
         start_free += total_bytes;
         return result;
     } else {
         size_t bytes_to_get = 2 * total_bytes + ROUND_UP(heap_size >> 4);
-        if (bytes_left > 0) {
+        if (bytes_left > 0) {  //内存里还有些零头，配置给适当的free-list中
             obj* volatile* my_free_list = freeLists + FREELIST_INDEX(bytes_left);
             ((obj*)start_free)->free_list_link = *my_free_list;
             *my_free_list = (obj*)start_free;
         }
+        //配置heap空间，用来补充内存池
         start_free = (char*)malloc(bytes_to_get);
-        if (0 == start_free) {
+        if (0 == start_free) {  //heap空间不足，malloc失败
             int i;
             obj *volatile *my_free_list, *p;
             /*系统实在没有内存了，向上释放链表已用内存*/
@@ -217,9 +222,11 @@ char *__default_alloc_template<threads, inst>::chunk_alloc(size_t size, int& nob
                     return chunk_alloc(size, nobjs);
                 }
             }
-            end_free = 0;
+            end_free = 0;  //到这里已经到山穷水尽，调用第一级配置器
             start_free = (char*)malloc_alloc::allocate(bytes_to_get);
+            //这会导致抛出异常或改善内存不足的情况
         }
+        //内存池补充足够，递归调用chunk_alloc()
         heap_size += bytes_left;
         end_free = start_free + bytes_to_get;
         return chunk_alloc(size, nobjs);
@@ -230,21 +237,21 @@ template<bool threads, int inst>
 void *__default_alloc_template<threads, inst>::refill(size_t n)
 {
     int nobjs = 20;
-    char *chunk = chunk_alloc(n, nobjs);
+    char *chunk = chunk_alloc(n, nobjs);  //获取20*n的区块，nobjs为引用会被chunk_alloc改变
     obj* volatile *my_free_list;
     obj* result;
     obj* current_obj, *next_obj;
     int i;
 
-    if (1 == nobjs) return chunk;
+    if (1 == nobjs) return chunk;  //如果只有一块，直接返回
     my_free_list = freeLists + FREELIST_INDEX(n);
-    result = (obj*)chunk;
-    *my_free_list = next_obj = (obj*)(chunk + n);
-    for (i = 1; ; i++) {
+    result = (obj*)chunk;  //取第一个块size大小的空间
+    *my_free_list = next_obj = (obj*)(chunk + n);  //指向第二块size空间
+    for (i = 1; ; i++) {  //循环将nobjs-1个size大小的空间依次插入到链表中
         current_obj = next_obj;
         next_obj = (obj*)((char*)next_obj + n);
-        if (nobjs - 1 == i) {
-            current_obj->free_list_link = 0;
+        if (nobjs - 1 == i) {  //此时已经将nbojs-1个size大小的空间插入完毕，退出循环
+            current_obj->free_list_link = 0;  
             break;
         } else {
             current_obj->free_list_link = next_obj;
@@ -262,13 +269,13 @@ void *__default_alloc_template<threads, inst>::reallocate(void *p,
     size_t copy_sz;
 
     if (old_sz > (size_t)MAX_BYTES && new_sz > (size_t)MAX_BYTES) {
-        return realloc(p, new_sz);
+        return realloc(p, new_sz);  //直接调用realloc()函数
     }
-    if (ROUND_UP(old_sz) == ROUND_UP(new_sz)) return p;
-    result = allocate(new_sz);
-    copy_sz = new_sz > old_sz ? old_sz : new_sz;
-    memcpy(result, p, copy_sz);
-    deallocate(p, old_sz);
+    if (ROUND_UP(old_sz) == ROUND_UP(new_sz)) return p;  //旧空间大小等于新空间大小，直接返回p
+    result = allocate(new_sz);  //调用allocate配置新空间
+    copy_sz = new_sz > old_sz ? old_sz : new_sz;  //取新旧空间大小的最小值
+    memcpy(result, p, copy_sz);  //复制copy_sz大小的内容到新空间
+    deallocate(p, old_sz);  //释放旧空间
     return result;
 }
 
